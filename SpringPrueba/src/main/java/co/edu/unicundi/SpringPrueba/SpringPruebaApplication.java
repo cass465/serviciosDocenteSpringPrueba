3179d66e441ae0305cb05888a8d8bc0eca8a915e
YEISON CIFUENTES <yeisoncifuentes2@gmail.com>
2021-04-16 18:24:32 -0500

    Servidor Tomcat

--- SpringPrueba/src/main/java/co/edu/unicundi/SpringPrueba/SpringPruebaApplication.java
+++ SpringPrueba/src/main/java/co/edu/unicundi/SpringPrueba/SpringPruebaApplication.java
@@ -2,12 +2,20 @@ package co.edu.unicundi.SpringPrueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringPruebaApplication {
+public class SpringPruebaApplication extends SpringBootServletInitializer{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPruebaApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(SpringPruebaApplication.class);
 	}
 
 }
